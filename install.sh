e466d3f74a48942fa054e4fa69af22f01037eb6e
Harjinder Dhanjal <malotian@gmail.com>
2020-02-17 04:01:16 +0000

    correct entry point

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ fission pkg delete --name ${name}
 fission env delete --name java
 fission env create --name java --image malotian/jvm-env --builder fission/jvm-builder --keeparchive --version 2
 fission pkg create --name ${name} --sourcearchive ${name}.zip --env java
-fission fn create --name java-echo-post-raw-body --pkg ${name} --entrypoint com.lingk.fission.api.JavaAddPost 
+fission fn create --name java-echo-post-raw-body --pkg ${name} --entrypoint com.lingk.fission.api.EchoPostRawBody 
 fission route create --name java-echo-post-raw-body --url /java-echo-post-raw-body --function java-echo-post-raw-body --method POST --createingress
